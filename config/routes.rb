0804db1bb916f77bb2b739daa2deaf13405514af
Kamal Hossain <mhssain1@memphis.edu>
2019-02-04 17:03:53 +0000

    added a home page'

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+  root to: 'pages#home'
+  get 'home', to: 'pages#home', as: 'home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
